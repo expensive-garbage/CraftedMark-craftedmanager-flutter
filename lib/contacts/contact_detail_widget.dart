846ed6f05eabd869d0ffb860744505cb8ed1b99b
Mark Nashed <mark@craftedlifestyle.co>
2023-04-29 14:37:31 -0700

    working v3 no update or save

--- lib/contacts/contact_detail_widget.dart
+++ lib/contacts/contact_detail_widget.dart
@@ -49,97 +49,104 @@ class ContactDetailWidget extends StatefulWidget {
 }
 
 class _ContactDetailWidgetState extends State<ContactDetailWidget> {
+  bool _editing = false;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('${widget.firstname} ${widget.lastname}'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Text(_editing ? 'Save' : 'Edit'),
+          onPressed: () {
+            setState(() {
+              _editing = !_editing;
+            });
+          },
+        ),
       ),
       child: SafeArea(
-        child: ListView(
-          children: [
-            SizedBox(height: 24),
-            Text(
-              '${widget.firstname} ${widget.lastname}',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CupertinoFormSection(
+                  header: Text('Basic Information'),
+                  children: [
+                    _buildFormRow('First Name', widget.firstname),
+                    _buildFormRow('Last Name', widget.lastname),
+                    _buildFormRow('Phone', widget.phone),
+                    _buildFormRow('Email', widget.email),
+                  ],
+                ),
+                CupertinoFormSection(
+                  header: Text('Address'),
+                  children: [
+                    _buildFormRow('Address 1', widget.address1),
+                    _buildFormRow('Address 2', widget.address2),
+                    _buildFormRow('City', widget.city),
+                    _buildFormRow('State', widget.state),
+                    _buildFormRow('ZIP', widget.zip),
+                  ],
+                ),
+                CupertinoFormSection(
+                  header: Text('Additional Information'),
+                  children: [
+                    _buildFormRow('Brand', widget.brand),
+                    _buildFormRow('Account Number', widget.accountNumber),
+                    _buildFormRow('Type', widget.type),
+                    _buildFormRowWithSwitch(
+                        'Customer-Based Pricing', widget.customerBasedPricing),
+                    _buildFormRow('Notes', widget.notes),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 24),
-            _buildContactActionRow('Phone', widget.phone, Icon(CupertinoIcons.phone)),
-            SizedBox(height: 8),
-            _buildContactActionRow('Email', widget.email, Icon(CupertinoIcons.mail)),
-            SizedBox(height: 24),
-            _buildAddressSection(),
-            SizedBox(height: 24),
-            _buildDetailSection(),
-            SizedBox(height: 24),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildContactActionRow(String label, String value, Icon icon) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              icon,
-              SizedBox(width: 12),
-              Text(label, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ],
           ),
-          Text(
-            value,
-            style: TextStyle(fontSize: 18),
-          ),
-        ],
+        ),
       ),
     );
-  }
-
-  Widget _buildAddressSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text('Address', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+  } // Add closing brace here for the class _ContactDetailWidgetState
+  Widget _buildFormRow(String label, String value) {
+    return CupertinoFormRow(
+      prefix: SizedBox(
+        width: 150,
+        child: Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        SizedBox(height: 8),
-        _buildInfoRow('${widget.address1}, ${widget.address2}, ${widget.city}, ${widget.state} ${widget.zip}'),
-      ],
-    );
-  }
-
-  Widget _buildDetailSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text('Details', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        ),
-        SizedBox(height: 8),
-        _buildInfoRow('Brand: ${widget.brand}'),
-        _buildInfoRow('Account Number: ${widget.accountNumber}'),
-        _buildInfoRow('Type: ${widget.type}'),
-        _buildInfoRow('Customer-Based Pricing: ${widget.customerBasedPricing}'),
-        _buildInfoRow('Notes: ${widget.notes}'),
-      ],
-    );
-  }
-
-  Widget _buildInfoRow(String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-      child: Text(
-        value,
-        style: TextStyle(fontSize: 16),
       ),
+      child: _editing
+          ? CupertinoTextFormFieldRow(
+        initialValue: value,
+        onChanged: (newValue) {},
+      )
+          : Text(value),
+    );
+  }
+
+  Widget _buildFormRowWithSwitch(String label, bool value) {
+    return CupertinoFormRow(
+      prefix: SizedBox(
+        width: 150,
+        child: Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      child: _editing
+          ? CupertinoSwitch(
+        value: value,
+        onChanged: (newValue) {
+          setState(() {
+            value = newValue;
+          });
+        },
+      )
+          : (value ? Text('Yes') : Text('No')),
     );
   }
 }
