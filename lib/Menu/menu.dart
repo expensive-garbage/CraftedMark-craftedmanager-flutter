2572a2a4ddc01fe82eead8541f6519e56f2a7e2e
Mark Nashed <mark@craftedlifestyle.co>
2023-05-08 11:41:09 -0700

    menu code addition'
    Signed-off-by: Mark Nashed <mark@craftedlifestyle.co>

--- lib/Menu/menu.dart
+++ lib/Menu/menu.dart
@@ -1,77 +1,110 @@
-import 'package:crafted_manager/Models/product_model.dart';
-import 'package:crafted_manager/Products/product_db_manager.dart';
+import 'package:crafted_manager/contacts/contact_lists.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-class MainMenuWidget extends StatelessWidget {
-  final String title;
-  final IconData iconData;
-  final VoidCallback onTap;
+import 'menu_item.dart';
 
-  const MainMenuWidget({
-    Key? key,
-    required this.title,
-    required this.iconData,
-    required this.onTap,
-  }) : super(key: key);
+class MenuView extends StatefulWidget {
+  const MenuView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(iconData),
-      title: Text(title),
-      onTap: onTap,
-    );
-  }
+  MenuViewState createState() => MenuViewState();
 }
 
-class MainMenu extends StatelessWidget {
-  final Function(Product) onMenuItemSelected;
+class MenuViewState extends State<MenuView> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
-  const MainMenu({
-    Key? key,
-    required this.onMenuItemSelected,
-  }) : super(key: key);
+  void _onMenuItemSelected(MenuItem menuItem) {
+    if (menuItem.destination != null) {
+      Navigator.push(
+        context,
+        CupertinoPageRoute(builder: (context) => menuItem.destination!),
+      );
+    }
+  }
+
+  final List<MenuItem> menuItems = [
+    MenuItem(title: "Dashboard", iconData: CupertinoIcons.speedometer),
+    MenuItem(
+        title: "Production",
+        iconData: CupertinoIcons.gear_alt_fill,
+        subItems: [
+          MenuItem(
+              title: "Production Schedule", iconData: CupertinoIcons.calendar),
+          MenuItem(
+              title: "Production Reports",
+              iconData: CupertinoIcons.doc_text_fill),
+        ]),
+    MenuItem(
+        title: "Orders/Invoicing",
+        iconData: CupertinoIcons.cart_fill,
+        subItems: [
+          MenuItem(title: "Invoices", iconData: CupertinoIcons.calendar),
+          MenuItem(
+              title: "Open Orders", iconData: CupertinoIcons.doc_text_fill),
+        ]),
+    MenuItem(title: "Payments", iconData: CupertinoIcons.creditcard_fill),
+    MenuItem(
+        title: "Contacts",
+        iconData: CupertinoIcons.person_2_fill,
+        destination: const ContactsList(),
+        subItems: [
+          MenuItem(
+              title: "Customers",
+              iconData: CupertinoIcons.person_crop_circle_fill),
+          MenuItem(title: "Suppliers", iconData: CupertinoIcons.car_fill),
+          MenuItem(title: "Vendors", iconData: CupertinoIcons.building_2_fill),
+        ]),
+    MenuItem(
+        title: "Inventory",
+        iconData: CupertinoIcons.cube_box_fill,
+        subItems: [
+          MenuItem(
+              title: "Ingredient Inventory",
+              iconData: CupertinoIcons.clear_fill),
+          MenuItem(
+              title: "Packaging Inventory",
+              iconData: CupertinoIcons.shift_fill),
+        ]),
+    MenuItem(title: "Accounting", iconData: CupertinoIcons.chart_pie_fill),
+    MenuItem(title: "Employee", iconData: CupertinoIcons.person_crop_circle),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('Menu'),
-      ),
-      child: Builder(builder: (context) {
-        return CupertinoScrollbar(
-          child: FutureBuilder<List<Product>>(
-            future: ProductPostgres.getAllProducts(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                final products = snapshot.data;
-                if (products != null) {
-                  return ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final product = products[index];
-
-                      // Replace this with your custom product menu item widget
-                      return MainMenuWidget(
-                        title: product.name,
-                        iconData: Icons.arrow_right, // Add the IconData here
-                        onTap: () => onMenuItemSelected(product),
-                      );
-                    },
-                  );
-                } else {
-                  return const Text('No products available');
-                }
-              }
-            },
+      backgroundColor: CupertinoColors.systemBackground,
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(
+            largeTitle: Text('Crafted Manager'),
           ),
-        );
-      }),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                MenuItem menuItem = menuItems[index];
+                return GestureDetector(
+                  onTap: () => _onMenuItemSelected(menuItem),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Row(
+                      children: [
+                        Icon(menuItem.iconData),
+                        const SizedBox(width: 10),
+                        Text(menuItem.title),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              childCount: menuItems.length,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
