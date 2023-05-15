a5b3c9bb04cd69931e249ae6509a464ca418b678
Mark Nashed <mark@craftedlifestyle.co>
2023-05-15 14:56:37 -0700

    Built Notifications into main.dart
    Signed-off-by: Mark Nashed <mark@craftedlifestyle.co>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,55 @@
 import 'package:crafted_manager/menu/menu.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // This line is required for async operations in main.
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // await fetchAndStoreAllSchemas();
-  // await _loadAllSchemas();
+  await _initializeNotifications();
 
   runApp(const CraftedManager());
+}
+
+Future<void> _initializeNotifications() async {
+  // Initialize the plugin with the app icon for Android
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // Initialize the plugin with the settings for iOS
+  final DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings(
+          onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+
+  // Initialize the plugin with the settings for Linux
+  final LinuxInitializationSettings initializationSettingsLinux =
+      LinuxInitializationSettings(defaultActionName: 'Open notification');
+
+  // Combine the initialization settings for all platforms
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsDarwin,
+      macOS: initializationSettingsDarwin,
+      linux: initializationSettingsLinux);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+}
+
+Future<void> onDidReceiveLocalNotification(
+    int id, String? title, String? body, String? payload) async {
+  // Handle local notification received while the app is in the foreground
+}
+
+Future<void> onDidReceiveNotificationResponse(
+    NotificationResponse notificationResponse) async {
+  final String? payload = notificationResponse.payload;
+  if (payload != null) {
+    debugPrint('notification payload: $payload');
+  }
+  // Handle notification tapped and app opened from notification
 }
 
 class CraftedManager extends StatelessWidget {
