f911af3b062ec0d87e341b94469ff1fecdbbb06b
Mark Nashed <mark@craftedlifestyle.co>
2023-05-21 21:11:05 -0700

    Bug fixes and updates
    Signed-off-by: Mark Nashed <mark@craftedlifestyle.co>

--- lib/Orders/edit_order_screen.dart
+++ lib/Orders/edit_order_screen.dart
@@ -4,6 +4,7 @@ import 'package:crafted_manager/Models/people_model.dart';
 import 'package:crafted_manager/Models/product_model.dart';
 import 'package:crafted_manager/Products/product_db_manager.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 
 import '../../Orders/orders_db_manager.dart';
 import 'product_search_screen.dart';
@@ -28,12 +29,14 @@ class EditOrderScreen extends StatefulWidget {
 class _EditOrderScreenState extends State<EditOrderScreen> {
   late List<OrderedItem> _orderedItems;
   double _subTotal = 0.0;
+  String _status = '';
 
   @override
   void initState() {
     super.initState();
     _orderedItems = List.from(widget.orderedItems);
     _subTotal = calculateSubtotal();
+    _status = widget.order.orderStatus;
   }
 
   double calculateSubtotal() {
@@ -44,13 +47,27 @@ class _EditOrderScreenState extends State<EditOrderScreen> {
     );
   }
 
+  void editOrderedItem(int index) {
+    if (_orderedItems[index].quantity > 1) {
+      setState(() {
+        _orderedItems[index] = _orderedItems[index].copyWith(
+          quantity: _orderedItems[index].quantity - 1,
+        );
+        _subTotal = calculateSubtotal();
+      });
+    } else {
+      setState(() {
+        _orderedItems.removeAt(index);
+        _subTotal = calculateSubtotal();
+      });
+    }
+  }
+
   void addOrderedItem(Product product, int quantity) {
-    // Check if the product is already in the orderedItems list
     int existingIndex = _orderedItems
         .indexWhere((orderedItem) => orderedItem.productId == product.id);
 
     if (existingIndex != -1) {
-      // If the product exists, update the existing line
       setState(() {
         _orderedItems[existingIndex] = _orderedItems[existingIndex].copyWith(
           quantity: _orderedItems[existingIndex].quantity + quantity,
@@ -58,7 +75,6 @@ class _EditOrderScreenState extends State<EditOrderScreen> {
         _subTotal = calculateSubtotal();
       });
     } else {
-      // If the product doesn't exist, add a new line
       setState(() {
         _orderedItems.add(OrderedItem(
           id: _orderedItems.length + 1,
@@ -80,6 +96,7 @@ class _EditOrderScreenState extends State<EditOrderScreen> {
   Future<Order?> updateOrder() async {
     Order updatedOrder = widget.order.copyWith(
       totalAmount: _subTotal,
+      orderStatus: _status,
     );
 
     final result = await OrderPostgres.updateOrder(updatedOrder, _orderedItems);
@@ -89,103 +106,153 @@ class _EditOrderScreenState extends State<EditOrderScreen> {
     } else {
       return null;
     }
-    ;
   }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('Edit Order'),
-        trailing: GestureDetector(
-          onTap: () async {
-            final updatedOrder = await updateOrder();
-            if (updatedOrder != null) {
-              Navigator.pop(context, updatedOrder);
-            } else {
-              showCupertinoDialog(
-                context: context,
-                builder: (_) => CupertinoAlertDialog(
-                  title: const Text("Error"),
-                  content: const Text("Failed to update order."),
-                  actions: [
-                    CupertinoDialogAction(
-                      isDefaultAction: true,
-                      child: const Text("OK"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+    return CupertinoApp(
+      theme: const CupertinoThemeData(brightness: Brightness.dark),
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('Edit Order'),
+          trailing: GestureDetector(
+            onTap: () async {
+              final updatedOrder = await updateOrder();
+              if (updatedOrder != null) {
+                Navigator.pop(context, updatedOrder);
+              } else {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (_) => CupertinoAlertDialog(
+                    title: const Text("Error"),
+                    content: const Text("Failed to update order."),
+                    actions: [
+                      CupertinoDialogAction(
+                        isDefaultAction: true,
+                        child: const Text("OK"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
+            child: const Text(
+              "Save",
+              style: TextStyle(color: CupertinoColors.activeBlue),
+            ),
+          ),
+        ),
+        child: SafeArea(
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: CupertinoFormSection.insetGrouped(
+                  header: const Text('Order information'),
+                  children: [
+                    CupertinoTextFormFieldRow(
+                      prefix: const Text('Customer:'),
+                      enabled: false,
+                      initialValue: widget.customer.firstName +
+                          ' ' +
+                          widget.customer.lastName,
+                    ),
+                    CupertinoFormRow(
+                      prefix: const Text('Status:'),
+                      child: CupertinoSlidingSegmentedControl<String>(
+                        thumbColor: CupertinoColors.activeBlue,
+                        children: const <String, Widget>{
+                          'Pending': Text('Pending'),
+                          'In-Progress': Text('In-Progress'),
+                          'Completed': Text('Completed'),
+                        },
+                        groupValue: _status,
+                        onValueChanged: (String? value) {
+                          setState(() {
+                            _status = value ?? '';
+                          });
+                        },
+                      ),
                     ),
                   ],
                 ),
-              );
-            }
-          },
-          child: const Text(
-            "Save",
-            style: TextStyle(color: CupertinoColors.activeBlue),
-          ),
-        ),
-      ),
-      child: SafeArea(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: CupertinoButton.filled(
-                child: const Text('Add Item'),
-                onPressed: () async {
-                  List<Product> products =
-                      await ProductPostgres.getAllProducts();
-                  final result =
-                      await showCupertinoModalPopup<Map<String, dynamic>>(
-                    context: context,
-                    builder: (BuildContext context) =>
-                        ProductSearchScreen(products: products),
-                  );
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: CupertinoButton.filled(
+                  child: const Text('Add Item'),
+                  onPressed: () async {
+                    List<Product> products =
+                        await ProductPostgres.getAllProducts();
+                    final result =
+                        await showCupertinoModalPopup<Map<String, dynamic>>(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          ProductSearchScreen(products: products),
+                    );
 
-                  if (result != null) {
-                    addOrderedItem(result['product'], result['quantity']);
-                  }
+                    if (result != null) {
+                      addOrderedItem(result['product'], result['quantity']);
+                    }
+                  },
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: _orderedItems.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(_orderedItems[index].id.toString()),
+                    onDismissed: (direction) {
+                      setState(() {
+                        _orderedItems.removeAt(index);
+                        _subTotal = calculateSubtotal();
+                      });
+                    },
+                    background:
+                        Container(color: CupertinoColors.destructiveRed),
+                    child: Container(
+                      color: CupertinoColors.black,
+                      child: CupertinoFormRow(
+                        prefix: Row(
+                          children: [
+                            Text(_orderedItems[index].productName),
+                            const SizedBox(width: 8),
+                            Text('Qty: ${_orderedItems[index].quantity}'),
+                          ],
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            editOrderedItem(index);
+                          },
+                          child: const Icon(CupertinoIcons.minus_circled,
+                              color: CupertinoColors.destructiveRed),
+                        ),
+                      ),
+                    ),
+                  );
                 },
               ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: _orderedItems.length,
-              itemBuilder: (context, index) {
-                return Dismissible(
-                  key: Key(_orderedItems[index].id.toString()),
-                  onDismissed: (direction) {
-                    setState(() {
-                      _orderedItems.removeAt(index);
-                      _subTotal = calculateSubtotal();
-                    });
-                  },
-                  background: Container(color: CupertinoColors.destructiveRed),
-                  child: CupertinoListTile(
-                    title: Text(_orderedItems[index].productName),
-                    subtitle: Text('Qty: ${_orderedItems[index].quantity}'),
-                  ),
-                );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Subtotal:'),
-                      Text('\$${_subTotal.toStringAsFixed(2)}'),
-                    ],
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Subtotal:'),
+                        Text('\$${_subTotal.toStringAsFixed(2)}'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
