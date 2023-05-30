c06fa04441e15a9351884cd78395a0cc90cfea15
Temofeyy <temofeyy.20@gmail.com>
2023-05-30 18:56:16 +0300

    added onesignal api

---
+++ lib/services/one_signal_api.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+
+class OneSignalAPI {
+  static const _url = "https://onesignal.com/api/v1/notifications";
+  static const _restApiKey = "MzVhNWRjN2ItZDVmZC00ZTZiLTk5NzYtNzc2NDEyMTU2NWM2";
+  static const _appKey = "fed52d24-522d-4653-ae54-3c23d0a735ac";
+
+  static const String companyName = "CraftedManager";
+
+  static Future<void> sendNotification(String message, List<String> ids) async {
+    var uri = Uri.parse(_url);
+
+    var payload = {
+      "included_segments": ids,
+      "app_id": _appKey,
+      "contents": {
+        "en": message,
+      },
+      "name": companyName,
+    };
+    var headers = {
+      "accept": "application/json",
+      "Authorization": "Basic $_restApiKey",
+      "content-type": "application/json"
+    };
+    try {
+      var result = await http.post(uri, headers: headers, body: payload);
+
+      print( result.body.toString());
+    }catch(e){
+      print(e);
+    }
+
+  }
+}
